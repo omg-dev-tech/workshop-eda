a9fa54cc3efc7c4ab8592cba831d94b189513810
김한솔 <anfelal35@gmail.com>
2025-09-02 23:51:31 +0900

    백엔드 corelation 추가

---
+++ api-gateway/src/main/java/com/workshop/gateway/config/FilterConfig.java
@@ -0,0 +1,28 @@
+package com.workshop.gateway.config;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class FilterConfig implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        // TODO Auto-generated method stub
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        httpResponse.setHeader("Timing-Allow-Origin", "*");
+
+        chain.doFilter(request, httpResponse);
+    }
+    
+    
+}
--- api-gateway/src/main/java/com/workshop/gateway/config/WebConfig.java
+++ api-gateway/src/main/java/com/workshop/gateway/config/WebConfig.java
@@ -7,17 +7,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig {
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://34.64.57.167") // UI 주소
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowCredentials(true);
-            }
-        };
-    }
+  @Bean
+  public WebMvcConfigurer crsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override public void addCorsMappings(CorsRegistry r) {
+        r.addMapping("/**")
+         .allowedOrigins("http://34.64.57.167") // UI 주소
+         .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+         .allowedHeaders("Content-Type","Authorization",
+                         "X-INSTANA-T","X-INSTANA-S","X-INSTANA-L") // ← 중요
+         .exposedHeaders("Server-Timing") // ← 브라우저가 읽을 수 있게
+         .allowCredentials(false);
+      }
+    };
+  }
 }
